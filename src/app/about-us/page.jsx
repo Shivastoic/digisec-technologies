22b0179a3a6e564dfe71205103bbfa607edcf1d8
Shiva pandey <shivapandey9898@gmail.com>
2024-12-23 17:43:49 +0530

    done from my side

--- src/app/about-us/page.jsx
+++ src/app/about-us/page.jsx
@@ -1,5 +1,7 @@
 // IMPORT COMPONENTS
 import Container from "@/app/components/container/index"
+import Image from "next/image"
+import ServiceCard from "@/app/home/services/service-card/index"
 
 // METADATA FOR ABOUT US PAGE
 export const metadata = {
@@ -13,36 +15,125 @@ export const metadata = {
 // HARDCODED DATA
 const data = {
 
-	description_one: "Digisec Technologies, proudly present our dedication to safeguarding your digital dreams. As a leading Cyber Security Solutions provider, we offer a comprehensive range of services and products tailored to your needs.",
-	description_two: "Our journey begins by understanding your business objectives, for it is with this knowledge that we craft the perfect digital roadmap for you. Our world-class engagement and consultation services ensure that you are equipped to face any digital challenges that lie ahead.",
-	description_three: "Our team of Cyber Security Experts takes pride in specializing in various domains, from Data Security to Cloud Security. With their vigilance, we fortify your network, applications, data, and cloud infrastructure, shielding them from cyber- attacks that may seek to harm your success.",
 	description_four: "But our commitment to your digital protection doesn’t stop there. We also offer Cloud solutions, IT Solutions (DC & Edge IT), and Software Solutions to further enhance your online ventures.",
+	description_one: "Digisec Technologies, proudly present our dedication to safeguarding your digital dreams. As a leading Cyber Security Solutions provider, we offer a comprehensive range of services and products tailored to your needs.",
+	description_three: "Our team of Cyber Security Experts takes pride in specializing in various domains, from Data Security to Cloud Security. With their vigilance, we fortify your network, applications, data, and cloud infrastructure, shielding them from cyber- attacks that may seek to harm your success.",
+	description_two: "Our journey begins by understanding your business objectives, for it is with this knowledge that we craft the perfect digital roadmap for you. Our world-class engagement and consultation services ensure that you are equipped to face any digital challenges that lie ahead.",
+	src_one: "/assets/images/about/slider/cyber-security.png",
+	src_two: "/assets/images/about/slider/audit.jpg",
+	title_one: "Our Vision",
+	title_two: "Our Mission",
 
 }
+
+const cardData = [
+
+    {
+
+        src: "/assets/images/services/cyber-security.png",
+        link: "/services",
+        title: "Cyber Security Solutions",
+        description: "Implement both traditional and advanced cybersecurity solutions to comprehensively secure your users, network, applications, data, and mobile apps effectively.",
+
+    },
+    {
+
+        src: "/assets/images/services/cloud.jpg",
+        link: "/services",
+        title: "Digital Transformation & Cloud",
+        description: "Empowering businesses to seamlessly transition to the cloud and navigate their digital transformation through expert consultation and facilitation.",
+
+    },
+    {
+
+        src: "/assets/images/services/audit.jpg",
+        link: "/services",
+        title: "Security Consulting & Audit",
+        description: "Help organizations to meet regulatory compliance, industry standards liks ISO 27001, security assessment of your network, applications and cloud infrastructure",
+
+    },
+    {
+
+        src: "/assets/images/services/development.avif",
+        link: "/development",
+        title: "Development services",
+        description: "Empowering businesses to innovate and thrive with cutting-edge web and AI development services, crafting scalable, user-centric applications and intelligent solutions that drive efficiency, enhance user experience, and unlock new opportunities in the digital era.",
+
+    },
+
+]
 
 export default function AboutUs() {
 	
 	return (
 
 		<section className="pt-28 pb-10 px-4">
-			<div className="bg-neutral-100 rounded-xl w-full py-8 md:py-16">
+			<div className="bg-white/15 rounded-xl w-full py-8 md:py-16">
 				<Container>
-					<div className="flex flex-col gap-8">
-						<h2 className="text-4xl md:text-5xl text-neutral-700 font-sora font-semibold">
-							About 
-							<span className="text-4xl md:text-5xl text-blue_logo"> DIGI</span>
-							<span className="text-4xl md:text-5xl text-yellow_logo">SEC </span>
-							Technologies
-						</h2>
-						<p className="text-lg font-sora">
-                            We provide effective
-                            <span className="text-yellow_title"> Cloud Transformation, Cyber Security Solution and Consulting Services </span>
-                            to our Customers, spread across PAN India.
-                        </p>
-						<p className="text-lg font-sora">{ data.description_one }</p>
-						<p className="text-lg font-sora">{ data.description_two }</p>
-						<p className="text-lg font-sora">{ data.description_three }</p>
-						<p className="text-lg font-sora">{ data.description_four }</p>
+					<div className="flex flex-col gap-20">
+						<div className="flex flex-col gap-4">
+							<h2 className="text-4xl md:text-5xl font-sora font-semibold text-white">
+								About 
+								<span className="text-4xl md:text-5xl text-blue_logo"> DIGI</span>
+								<span className="text-4xl md:text-5xl text-yellow_logo">SEC </span>
+								Technologies
+							</h2>
+							<p className="text-lg font-sora text-white/90">
+								We provide effective
+								<span className="text-yellow_title"> Cloud Transformation, Cyber Security Solution and Consulting Services </span>
+								to our Customers, spread across PAN India.
+							</p>
+						</div>
+						<div className="flex flex-col gap-10">
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-6 pb-6 items-center">
+								<div className="flex flex-col gap-4">
+									<h3 className="text-3xl md:text-4xl font-sora font-semibold text-yellow_title">{ data.title_one }</h3>
+									<p className="text-lg font-sora text-white/90">{ data.description_one }</p>
+									<p className="text-lg font-sora text-white/90">{ data.description_two }</p>
+								</div>
+								<Image 
+									src={ data.src_one }
+									alt=""
+									width={ 1000 }
+									height={ 1000 }
+									className="w-full h-full rounded-lg"
+								/>
+							</div>
+							<div className="grid grid-cols-1 md:grid-cols-2 gap-6 pb-6 items-center">
+								<Image 
+									src={ data.src_two }
+									alt=""
+									width={ 1000 }
+									height={ 1000 }
+									className="w-full h-full rounded-lg"
+								/>
+								<div className="flex flex-col gap-4">
+									<h3 className="text-3xl md:text-4xl font-sora font-semibold text-yellow_title">{ data.title_two }</h3>
+									<p className="text-lg font-sora text-white/90">{ data.description_three }</p>
+									<p className="text-lg font-sora text-white/90">{ data.description_four }</p>
+								</div>
+							</div>
+						</div>
+						<div className="flex flex-col gap-12 items-center justify-center">
+							<h4 className="text-4xl md:text-5xl font-sora font-semibold text-white">Services we Offer</h4>
+							<div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+								{
+		
+									cardData.map( ( data, index ) => (
+		
+										<ServiceCard
+											key={ index }
+											src={ data.src }
+											title={ data.title }
+											description={ data.description }
+											link={ data.link }
+										/>
+		
+									))
+		
+								}
+							</div>
+						</div>
 					</div>
 				</Container>
 			</div>
--- src/app/contact-us/contact-form/index.jsx
+++ src/app/contact-us/contact-form/index.jsx
@@ -9,14 +9,14 @@ export default function ContactForm(){
                     required 
                     name="fname"
                     placeholder="First Name"
-                    className="borde px-4 py-2 placeholder:text-white/80 text-white bg-white/30 rounded-lg w-full focus:outline-none focus:ring-1 focus:ring-white"
+                    className="border-2 border-white/20 px-4 py-2 placeholder:text-white/80 text-white bg-white/20 rounded-lg w-full focus:outline-none focus:ring-1 focus:ring-white"
                 />
                 <input 
                     type="text" 
                     required 
                     name="lname"
                     placeholder="Last Name"
-                    className=" px-4 py-2 placeholder:text-white/80 text-white bg-white/30 rounded-lg w-full focus:outline-none focus:ring-1 focus:ring-white"
+                    className="border-2 border-white/20 px-4 py-2 placeholder:text-white/80 text-white bg-white/20 rounded-lg w-full focus:outline-none focus:ring-1 focus:ring-white"
                 />
             </div>
             <input 
@@ -24,22 +24,33 @@ export default function ContactForm(){
                 required 
                 name="email"
                 placeholder="Email"
-                className=" px-4 py-2 placeholder:text-white/80 text-white bg-white/30 rounded-lg focus:outline-none focus:ring-1 focus:ring-white"
+                className="border-2 border-white/20 px-4 py-2 placeholder:text-white/80 text-white bg-white/20 rounded-lg focus:outline-none focus:ring-1 focus:ring-white"
             />
-            <input 
-                type="number" 
-                required 
-                name="phone"
-                placeholder="Phone No."
-                className=" px-4 py-2 placeholder:text-white/80 text-white bg-white/30 rounded-lg focus:outline-none focus:ring-1 focus:ring-white"
-            />
+            <div className="flex gap-4">
+                <input 
+                    type="number" 
+                    required 
+                    name="phone"
+                    placeholder="Phone No."
+                    className="flex-1 border-2 border-white/20 px-4 py-2 placeholder:text-white/80 text-white bg-white/20 rounded-lg focus:outline-none focus:ring-1 focus:ring-white"
+                />
+                <select 
+					name="Interested" 
+					required
+					className="flex-1 border-2 border-white/20 rounded-lg px-4 py-2 w-full bg-white/20 placeholder:text-white/80 text-white focus:outline-none focus:ring-1 focus:ring-white"
+				>
+					<option className='text-black' value="">Select Interest</option>
+					<option className='text-black' value="Courses">Courses</option>
+					<option className='text-black' value="Services">Services</option>
+				</select>
+            </div>
             <textarea 
                 type="number" 
                 required 
                 name="message"
                 placeholder="Message"
                 rows={ 5 }
-                className=" px-4 py-2 bg-white/30 placeholder:text-white/80 text-white rounded-lg resize-none focus:outline-none focus:ring-1 focus:ring-white"
+                className="border-2 border-white/20 px-4 py-2 bg-white/20 placeholder:text-white/80 text-white rounded-lg resize-none focus:outline-none focus:ring-1 focus:ring-white"
             />
             <button className="px-6 py-2 border-2 hover:bg-blue-500 text-white duration-300 border-white rounded-full w-fit font-medium font-sora">Submit</button>
         </form>
